fd3bad2d865f06ddb82cf36a5891a1cda7ce81b1
Eddie Allen <eddieallen84@github.com>
2023-12-06 17:06:45 -0400

    desenvolvimento web javascript

--- aula/01 - variaveis e operadores.js
+++ aula/01 - variaveis e operadores.js
@@ -3,10 +3,16 @@ const precoEtanol = 3.30;
 let kmPorLitroGasolina = 11.2;
 let kmPorLitroEtanol = 8.7;
 let distanciaEmKm = 700;
+let combustivel = 'gasolina';
 
 let custoViagemGasolina = distanciaEmKm / kmPorLitroGasolina * precoGasolina;
 let custoViagemEtanol = distanciaEmKm / kmPorLitroEtanol * precoEtanol;
 
-console.log("Custo da viagem com gasolina R$" , custoViagemGasolina.toFixed(2));
-console.log("Custo da viagem com etanol R$" , custoViagemEtanol.toFixed(2));
-
+if (combustivel === 'gasolina') {
+    console.log("Custo da viagem com gasolina R$" , custoViagemGasolina.toFixed(2));
+} else if (combustivel === 'etanol') {
+    console.log("Custo da viagem com etanol R$" , custoViagemEtanol.toFixed(2));
+} 
+else {
+    console.log('tipo de combustível inválido');
+}
