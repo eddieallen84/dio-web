53e14c16929464d0404298a830772340bf6c9fb4
Eddie Allen <eddieallen84@github.com>
2023-12-06 17:12:29 -0400

    javascript

--- aula/02 - condicionais.js
+++ aula/02 - condicionais.js
@@ -2,9 +2,9 @@ const number = 15;
 
 const isPair = (number % 2) === 0;
 
-if (number === 0){
+if (number === 0) {
     console.log('O número é inválido')
-} else if (isPair){
+} else if (isPair) {
     console.log('O número é par!')
 }
     else {
