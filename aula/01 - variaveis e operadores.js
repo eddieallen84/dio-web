79874ebf773c72c57b9cb908262f5d1d5f7e0999
Eddie Allen <eddieallen84@github.com>
2023-12-07 09:24:04 -0400

    java script

--- aula/01 - variaveis e operadores.js
+++ aula/01 - variaveis e operadores.js
@@ -1,18 +1,20 @@
 const precoGasolina = 5.63;
-const precoEtanol = 3.30;
+const precoEtanol = 3.3;
 let kmPorLitroGasolina = 11.2;
 let kmPorLitroEtanol = 8.7;
 let distanciaEmKm = 700;
-let combustivel = 'gasolina';
+let combustivel = "gasolina";
 
-let custoViagemGasolina = distanciaEmKm / kmPorLitroGasolina * precoGasolina;
-let custoViagemEtanol = distanciaEmKm / kmPorLitroEtanol * precoEtanol;
+let custoViagemGasolina = (distanciaEmKm / kmPorLitroGasolina) * precoGasolina;
+let custoViagemEtanol = (distanciaEmKm / kmPorLitroEtanol) * precoEtanol;
 
-if (combustivel === 'gasolina') {
-    console.log("Custo da viagem com gasolina R$" , custoViagemGasolina.toFixed(2));
-} else if (combustivel === 'etanol') {
-    console.log("Custo da viagem com etanol R$" , custoViagemEtanol.toFixed(2));
-} 
-else {
-    console.log('tipo de combustível inválido');
+if (combustivel === "gasolina") {
+  console.log(
+    "Custo da viagem com gasolina R$",
+    custoViagemGasolina.toFixed(2)
+  );
+} else if (combustivel === "etanol") {
+  console.log("Custo da viagem com etanol R$", custoViagemEtanol.toFixed(2));
+} else {
+  console.log("tipo de combustível inválido");
 }
--- aula/02 - condicionais.js
+++ aula/02 - condicionais.js
@@ -1,12 +1,11 @@
 const number = 15;
 
-const isPair = (number % 2) === 0;
+const isPair = number % 2 === 0;
 
 if (number === 0) {
-    console.log('O número é inválido')
+  console.log("O número é inválido");
 } else if (isPair) {
-    console.log('O número é par!')
+  console.log("O número é par!");
+} else {
+  console.log("O número é ímpar");
 }
-    else {
-        console.log('O número é ímpar')
-    }
