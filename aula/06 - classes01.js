e4557cc321bc4a0583658febed1e353bfe694583
eddieallen84 <eddieallen84@github.com>
2023-12-16 18:07:12 -0400

    javascript

--- aula/06 - classes01.js
+++ aula/06 - classes01.js
@@ -1,3 +1,29 @@
 class Pessoa {
+  nome;
+  idade;
 
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    this.anoDeNascimento = 2023 - idade;
+  }
+
+  descrever() {
+    console.log(`Meu nome é ${this.nome} e eu tenho ${this.idade} anos`);
+  }
 }
+
+function compararPessoas(p1, p2) {
+  if (p1.idade > p2.idade) {
+    console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+  } else if (p2.idade > p1.idade) {
+    console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+  } else {
+    console.log(`${p1.nome} e ${p2.nome} têm a mesma idade`);
+  }
+}
+
+const eddie = new Pessoa("Eddie", 39);
+const marcelle = new Pessoa("Marcelle", 34);
+
+compararPessoas(eddie, marcelle);
