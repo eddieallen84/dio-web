bb2bbcc6ea6e2bd54de0e8495370dececa72e99c
Eddie Allen <eddieallen84@github.com>
2023-12-19 14:04:19 -0400

    javascript

---
+++ aula/07 - arrays.js
@@ -0,0 +1,13 @@
+const aluno = ["Eddie", "Marcelle", "Kátia"];
+
+aluno.push("Carlos");
+
+aluno[4] = "Vinícius";
+aluno[0] = "Aline";
+
+console.log(aluno);
+console.log(aluno[4]);
+aluno.pop();
+console.log(aluno);
+aluno.shift();
+console.log(aluno);
