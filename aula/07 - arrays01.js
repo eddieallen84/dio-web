7c11d79a8ba077f307317f7eb1496b070adff7a3
Eddie Allen <eddieallen84@github.com>
2024-01-16 13:53:47 -0400

    js

--- aula/07 - arrays01.js
+++ aula/07 - arrays01.js
@@ -6,9 +6,10 @@ notas.push(8);
 notas.push(2);
 notas.push(5);
 
-const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4]
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+
+console.log(soma / 5);
 
 for (let i = 0; i < 10; i++) {
-    console.log(i);    
-    
+  console.log(i);
 }
