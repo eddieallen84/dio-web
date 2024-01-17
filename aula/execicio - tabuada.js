262e3ec4778a4a09525df9d79672885b22dd25ba
Eddie Allen <eddieallen84@github.com>
2024-01-17 16:49:05 -0400

    js

--- aula/execicio - tabuada.js
+++ aula/execicio - tabuada.js
@@ -1,4 +1,5 @@
-function tabuada(numero) {
+
+function tabuada(numero){
   console.log(`\n**** TABUADA DE ${numero} ****\n`);
   for (let i = 0; i < 11; i++) {
     console.log(`${numero} x ${i} = ${numero * i}`);
